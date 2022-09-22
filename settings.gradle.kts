0db4f3f58badd17a0d62a4dda0d09260764504ef
Yuniel Acosta Pérez <33158051+yacosta738@users.noreply.github.com>
2022-09-21 23:09:10 -0300

    add astro frontend

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,7 +13,31 @@ pluginManagement {
     }
 }
 rootProject.name="quarkomment"
-include("web")
-include("domain")
-include("port")
+
+val apps = File("apps")
+val libs = File("libs")
+
+loadSubProjects(listOf(apps, libs))
+
+fun loadSubProjects(modules: List<File>) {
+    modules.forEach { module ->
+        if (module.exists()) {
+            if (module.isDirectory) {
+                module.listFiles()?.forEach { submodule ->
+                    if (submodule.isDirectory) {
+                        println("Loading submodule \uD83D\uDCE6: ${submodule.name}")
+                        include(":${submodule.name}")
+                        project(":${submodule.name}").projectDir = File("${module.name}/${submodule.name}")
+                    } else {
+                        println("${submodule.name} is not a directory \uD83D\uDDFF - skipping")
+                    }
+                }
+            } else {
+                println("${module.name} is not a directory \uD83D\uDE12 - ${module.name}")
+            }
+        } else {
+            println("${module.name} directory does not exist \uD83D\uDEAB - ${module.name}")
+        }
+    }
+}
 
